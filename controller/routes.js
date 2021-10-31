ea4bd260b1caa4958263253990675b02b1f76825
Irit Kushwaha <iritkushwaha2607@gmail.com>
2021-10-31 19:45:51 +0530

    google auth done

--- controller/routes.js
+++ controller/routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const user = require('../model/user');
-const bcryptjs = require('bcryptjs')
+const bcryptjs = require('bcryptjs');
 const passport = require('passport');
 require('./passportLocal')(passport);
-
+require('./googleAuth')(passport);
 
 function checkAuth(req, res, next) {
     if (req.isAuthenticated()) {
@@ -16,7 +16,7 @@ function checkAuth(req, res, next) {
     }
 }
 
-router.get('/',(req,res) => {
+router.get('/', (req, res) => {
     if (req.isAuthenticated()) {
         res.render("index", { logged: true });
     } else {
@@ -24,68 +24,57 @@ router.get('/',(req,res) => {
     }
 });
 
-router.get('/login',(req,res) => {
-    res.render("login");
+router.get('/login', (req, res) => {
+    res.render("login", { csrfToken: req.csrfToken() });
 });
 
-router.get('/signup',(req,res) => {
-    res.render("signup", { });
+router.get('/signup', (req, res) => {
+    res.render("signup", { csrfToken: req.csrfToken() });
 });
 
-router.get('/profile',(req,res) => {
-    res.render('profile');
-});
+router.post('/signup', (req, res) => {
+    // get all the values 
+    const { email, username, password, confirmpassword } = req.body;
+    // check if the are empty 
+    if (!email || !username || !password || !confirmpassword) {
+        res.render("signup", { err: "All Fields Required !", csrfToken: req.csrfToken() });
+    } else if (password != confirmpassword) {
+        res.render("signup", { err: "Password Don't Match !", csrfToken: req.csrfToken() });
+    } else {
 
-router.post('/signup',(req,res) => {
-    // get all the values
-    const {email, username, password, confirmpassword } = req.body;
-
-    // check if the one empty
-
-    if(!email || !username || !password || !confirmpassword){
-        res.render("signup", {err : "All fields are required" });
-    } else if(password != confirmpassword){
-        res.render("signup", {err :"Password don't match"});
-    }else{
-        // res.render("signup", {err:"All set"});
-       
-        user.findOne({ $or : [ {email:email}, {username : username} ] }, function(err,data){
-            if(err) throw err;
-            if(data){
-                res.render("signup", {err :"User Exists!! Logging in"});
+        // validate email and username and password 
+        // skipping validation
+        // check if a user exists
+        user.findOne({ $or: [{ email: email }, { username: username }] }, function (err, data) {
+            if (err) throw err;
+            if (data) {
+                res.render("signup", { err: "User Exists, Try Logging In !", csrfToken: req.csrfToken() });
+            } else {
+                // generate a salt
+                bcryptjs.genSalt(12, (err, salt) => {
+                    if (err) throw err;
+                    // hash the password
+                    bcryptjs.hash(password, salt, (err, hash) => {
+                        if (err) throw err;
+                        // save user in db
+                        user({
+                            username: username,
+                            email: email,
+                            password: hash,
+                            googleId: null,
+                            provider: 'email',
+                        }).save((err, data) => {
+                            if (err) throw err;
+                            // login the user
+                            // use req.login
+                            // redirect , if you don't want to login
+                            res.redirect('/login');
+                        });
+                    })
+                });
             }
-            else
-            {
-                   //generate a salt
-                   bcryptjs.genSalt(12,(err,salt) => {
-                       if(err) throw err;
-                       //hash the password
-                       bcryptjs.hash(password, salt, (err,hash) => {
-                           if(err) throw err;
-                           user({
-                               username: username,
-                               email : email,
-                               password : hash,
-                               googleId: null,
-                               provide: 'email',
-                            
-                           }).save((err, data) => {
-                               if(err) throw err;
-                               res.redirect('/login')
-                           });
-                       })
-                   });
-            }
-        })
-
+        });
     }
-});
-
-router.get('/logout', (req, res) => {
-    req.logout();
-    req.session.destroy(function (err) {
-        res.redirect('/');
-    });
 });
 
 router.post('/login', (req, res, next) => {
@@ -96,11 +85,24 @@ router.post('/login', (req, res, next) => {
     })(req, res, next);
 });
 
-router.get('/profile', checkAuth, (req,res) => {
-   
-        res.render('profile',{ username: req.user.username });
-    
-   
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.session.destroy(function (err) {
+        res.redirect('/');
+    });
 });
+
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email',] }));
+
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
+    res.redirect('/profile');
+});
+
+router.get('/profile', checkAuth, (req, res) => {
+    // adding a new parameter for checking verification
+    res.render('profile', { username: req.user.username, verified : req.user.isVerified });
+
+});
+
 
 module.exports = router;
